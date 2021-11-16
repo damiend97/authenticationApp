eff9fe4fa883681fe1cddf9a71980b44d4006ff5
damiend97 <durandamien1997@gmail.com>
2021-11-16 15:47:46 -0700

    added functionality

--- src/index.js
+++ src/index.js
@@ -1,5 +1,63 @@
 // index.js
+// obviously not much for error handling but it works!
+
+// console
+// https://www.npoint.io/docs/793aedf7ca718c314548
+
+// bin
+// https://api.npoint.io/793aedf7ca718c314548
 
 import './styles/main.scss';
+import { userInput, passInput, submitButton } from './js/domLoader';
 
-console.log("Welcome to the auth app!");
+submitButton.addEventListener('click', () => {
+    // userCredentials
+    let userCredentials = {
+        username: userInput.value,
+        password: passInput.value
+    }
+
+    fetchDB()
+    .then((loadedDatabase) => {
+        console.log("fetchDB :", loadedDatabase)
+        return loginUser(loadedDatabase.users, userCredentials);
+    })
+    .then((loadedUser) => {
+        console.log("loginUser : ", loadedUser);
+        return displayUser(loadedUser);
+    })
+})
+
+const fetchDB = async () => {
+    try {
+        let userDB = await fetch('https://api.npoint.io/793aedf7ca718c314548');
+
+        if(!userDB.ok) {
+            throw new Error("Database not found");
+        }
+
+        // loadedDatabase
+        return userDB.json();
+    } catch (err) {
+        return err;
+    }
+}
+
+const loginUser = async (database, user) => {
+    for (let i = 0; i < database.length; i++) {
+        let dbUser = database[i];
+
+        if(dbUser.username === user.username && dbUser.password === user.password) {
+            return dbUser;
+        }
+    }    
+}
+
+const displayUser = async (user) => {
+    document.querySelector('body').classList.add("flexCenter");
+    document.querySelector('body').classList.add("fullScreen");
+    document.querySelector('body').classList.add("whiteText");
+    document.querySelector('body').innerHTML = (
+        "<div><div>Welcome, " + user.username + "</div><br><div>YOUR COLOR IS...</div><br><div>" + user.color + "</div></div>"
+    );
+}
---
+++ src/js/domLoader.js
@@ -0,0 +1,5 @@
+// domLoader.js
+
+export var userInput = document.querySelector('#userInput');
+export var passInput = document.querySelector('#passInput');
+export var submitButton = document.querySelector('#submit');
